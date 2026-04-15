bf1cc6a8b7b56e42263517f4711ae692f5c4dcd1
Vitor Hugo <prange.vitor@gmail.com>
2026-04-15 14:10:05 -0300

    feitos

--- assets/js/logica-04-metodos-string-exercicios.js
+++ assets/js/logica-04-metodos-string-exercicios.js
@@ -17,13 +17,105 @@ function converterParaMinusculas(){
 }
 
 function mostrarPrimeiraLetra(){
-    let frase = prompt("Digite a frase");
+    let palavra = prompt("Digite a palavra");
 
-    alert("A primeira letra: " + frase[0]);
+    alert("A primeira letra: " + palavra[0]);
 }
 
 function mostrarUltimaLetra(){
+    let palavra = prompt("Digite a palavra");
+
+    alert("A ultima letra: " + palavra[palavra.length - 1]);
+}
+
+function verificarPalavraNaFrase(){
+    let frase = prompt("Digite a frase");
+    let palavra = prompt("Digite a palavra");
+
+    if(frase.includes(palavra) === true){
+        alert("A palavra " + palavra + " aparece na frase: " + frase);
+    }else{
+        alert("A palavra " + palavra + " não aparece na frase: " + frase);
+    }
+}
+
+function substituirCorNaFrase(){
     let frase = prompt("Digite a frase");
 
-    alert("A ultima letra: " + frase[frase.length - 1]);
+    frase = frase.toLocaleLowerCase().replace("azul", "vermelho");
+
+    alert(frase);
+}
+
+function trocarEstacao(){
+    let frase = "Eu gosto de verão"
+
+    frase = frase.toLocaleLowerCase().replace("verão", "inverno");
+
+    alert(frase);
+}
+
+function mostrarQuatroPrimeirosCaracteres(){
+    let palavra = prompt("Digite a palavra");
+
+    let quatroletras = palavra.slice(0, 4);
+
+    alert(quatroletras);
+}
+
+function separarNomeCompleto(){
+    let nomeCompleto = prompt("Digite o nome completo");
+
+    let separados = nomeCompleto.split(" ");
+
+    alert(separados.join(" "));
+}
+
+function removerEspacosExtras(){
+    let frase = prompt("Digite a frase");
+
+    alert(frase.trim());
+}
+
+
+function repetirPalavraTresVezes(){
+    let palavra = prompt("Digite a palavra");
+
+    alert(palavra.repeat(3));
+
+}
+
+
+function encontrarPosicaoPalavra(){
+    let frase = "Eu estudo JavaScript todos os dias";
+
+    let posicao = frase.indexOf("JavaScript");
+
+    alert("Posição: " + posicao);
+}
+
+function verificarInicioEFim(){
+    let palavra = prompt("Digite a palavra");
+
+    if((palavra.startsWith("a") === true) && (palavra.endsWith("o") === false)){
+        alert("Começa com a letra 'a' ");
+    }
+    else if((palavra.startsWith("a") === false) && (palavra.endsWith("o") === true)){
+        alert("Termina com a letra 'o'");
+    }
+    else if((palavra.startsWith("a") === true) && (palavra.endsWith("o") === true)){
+        alert("Começa com 'a' e termina com 'o'");
+    }
+    else{
+        alert("não começa com 'a' nem termina com 'o'");
+    }
+    
+}
+
+function trocarDominioEmail(){
+    let email = prompt("Digite o Email");
+
+    email = email.replace("@gmail.com", "@hotmail.com");
+
+    alert(email);
 }
