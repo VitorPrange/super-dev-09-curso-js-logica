dbe531cf5572404a427cd3883ff97daeaac58b8d
Vitor Prange <12965@proway.treina>
2026-04-24 21:55:35 -0300

    ate 53

--- assets/js/logica-06-for-exercicios.js
+++ assets/js/logica-06-for-exercicios.js
@@ -105,7 +105,7 @@ function exercicio13ContarLetrasNomes(){
 }
 
 function exercicio14MaiorNumero(){
-    let maiorNumero = -Infinity;
+    let maiorNumero = 0;
     for(let indice = 0; indice < 5; indice = indice + 1){
         let numero = parseInt(prompt("Digite o numero"));
 
@@ -250,8 +250,8 @@ function exercicio25ContarNotasAcimaSete(){
 }
 
 function exercicio26MaiorEMenorNumero(){
-    let maiorNumero = -Infinity;
-    let menorNumero = +Infinity;
+    let maiorNumero = 0;
+    let menorNumero = 9999999999999;
 
     for(let index = 0; index < 7; index = index + 1){
         let numero = parseFloat(prompt("Digite o numero"));
@@ -479,8 +479,294 @@ function exercicio40RelatorioCompletoTurmaSemVetor(){
 
         let media = (nota1 + nota2) / 2;
 
-        if(media)[
-            
-        ]
+        if(media >= 7){
+            somaAlunosAprovados = somaAlunosAprovados + 1;
+        }else if(media < 7){
+            somaAlunosReprovados = somaAlunosReprovados + 1;
+        }
+
+        if(media > maiorMediaTurma){
+            maiorMediaTurma = media;
+            nomeAlunoComMaiorMedia = nome;
+        }
+
+        if(media < menorMediaTurma){
+            menorMediaTurma = media;
+        }
+    }
+}
+
+function exercicio41ForTabuadaIntervalo(){
+    let inicio = parseInt(prompt("Digite o inicio"));
+    let final = parseInt(prompt("Digite o final"));
+
+    for(let indice = inicio; indice < final; indice = indice + 1){
+        for(let indice2 = 1; indice2 <= 10; indice2 = index2 + 1){
+            console.log(indice * indice2);
+        }
+    }
+}
+
+function exercicio42ForContarDivisores(){
+    let numero = parseInt(prompt("Digite o numero"));
+
+    for(let indice = 1; indice <= numero; indice = indice + 1){
+        if(numero % indice === 0){
+            console.log(indice);
+        }
+    }
+}
+
+function exercicio43ForVerificarPrimo(){
+    let numero = parseInt(prompt("Digite o numero"));
+    let contadorPrimo = 0;
+
+    for(let indice = 1; indice <= numero; indice = indice + 1){
+        if(numero % indice === 0){
+            contadorPrimo = contadorPrimo + 1;
+        }
+    }
+
+    if(contadorPrimo > 2){
+        console.log("Número é primo");
+    }else{
+        console.log("Número não é primo")
+    }
+}
+
+function exercicio44ForSomarFatoriais(){
+    let numero = parseInt(prompt("Digite o numero"));
+    let resultado = 1;
+
+    for(let indice = 1; indice <= numero; indice = indice + 1){
+        resultado = resultado * indice;
+    }
+}
+
+function exercicio45ForMediaIdadesComFaixas(){
+    let pessoas012 = 0;
+    let pessoas1317 = 0;
+    let pessoas1859 = 0;
+    let pessoas60Mais = 0;
+    let somaIdade = 0;
+
+    for(let index = 0; index < 10; index = index + 1){
+        let idade = parseInt(prompt("Digite a sua idade"));
+        
+        somaIdade = somaIdade + idade;
+
+
+        if(idade > 0 && idade <= 12){  
+            pessoas012 = pessoas012 + 1;
+        }
+        else if(idade > 12 && idade <= 17){
+            pessoas1317 = pessoas1317 + 1;
+        }
+        else if(idade > 17 && idade <= 59){
+            pessoas1859 = pessoas1859 + 1;
+        }
+        else if(idade >= 60){
+            pessoas60Mais = pessoas60Mais + 1;
+        }
+    }
+    let mediaIdades = somaIdade / 10;
+
+    console.log(
+        "Pessoas entre 0 e 12 anos: " + pessoas012 + "\n" +
+        "Pessoas entre 13 e 17 anos: " + pessoas1317 + "\n" +
+        "Pessoas entre 18 e 59 anos: " + pessoas1859 + "\n" +
+        "Pessoas com mais de 60 anos: " + pessoas60Mais
+    );
+
+}
+
+function exercicio46ForMaiorMenorComValidacao(){
+    let maiorNumero = 0;
+    let menorNumero = 999999999999;
+    for(let indice = 0; indice < 8; indice = indice + 1){
+        let numero = parseInt(prompt("Digite o numero"));
+
+        while(numero < 0){
+            numero = parseInt(prompt("Numero invalido, valor não pode ser negativo, digite o numero novamente"));
+        }
+
+        if(numero > maiorNumero){
+            maiorNumero = numero
+        }
+        if(numero < menorNumero){
+            menorNumero = numero
+        }
+    }
+    let diferençaMaiorMenor = maiorNumero - menorNumero;
+    console.log("Maior numero: " + maiorNumero + "\n" +
+        "Menor numero: " + menorNumero + "\n" +
+        "Diferença entre o maior e o menor numero: " + diferençaMaiorMenor
+    );
+}
+
+function exercicio47ForMediaNotasTurmas(){
+    let somaA = 0;
+    let somaB = 0;
+    let maiorMedia = 0;
+    let nomeMaiorMedia;
+    for(let index = 0; index < 5; index = index + 1){
+        let notaAluno = prompt("Digite a nota do aluno")
+
+        somaA = somaA + notaAluno;
+    }
+    let mediaA = somaA / 5;
+
+    for(let index = 0; index < 5; index = index + 1){
+        let notaAluno = prompt("Digite a nota do aluno")
+
+        somaB = somaB + notaAluno;
+    }
+    let mediaB = somaB / 5;
+
+    if(mediaA > mediaB){
+        maiorMedia = mediaA
+        nomeMaiorMedia = "Turma A"
+    }else if(mediaB > mediaA){
+        maiorMedia = mediaB
+        nomeMaiorMedia = "Turma B"
+    }else if(mediaA === mediaB){
+        nomeMaiorMedia = "Empate de medias"
+    }
+
+    alert(
+        "Media turma A: " + mediaA + "\n" +
+        "Media turma B: " + mediaB + "\n" +
+        "Turma com maior media: " + nomeMaiorMedia
+    )
+}
+
+function exercicio48ForRelatorioEleicao(){
+    let voto1 = 0;
+    let voto2 = 0;
+    let voto3 = 0;
+    let voto4 = 0;
+    for(let index = 0; index < 12; index = index + 1){
+        let voto = parseInt(prompt("Vote em uma das opções: " + "\n" +
+            "1 - Candidato A" + "\n" + 
+            "2 - Candidato B" + "\n" +
+            "3 - Branco" + "\n" +
+            "4 - Nulo"
+        ));
+
+        if(voto !== "1" || voto !== "2" || voto !== "3" || voto !== "4"){
+            voto = parseInt(prompt("Opção invalida, vote em uma das opções: " + "\n" +
+                "1 - Candidato A" + "\n" + 
+                "2 - Candidato B" + "\n" +
+                "3 - Branco" + "\n" +
+                "4 - Nulo"
+            ));
+        }
+
+        if(voto === "1"){
+            voto1 = voto1 + 1;
+        }
+        if(voto === "2"){
+            voto2 = voto2 + 1;
+        }
+        if(voto === "3"){
+            voto3 = voto3 + 1;
+        }
+        if(voto === "4"){
+            voto4 = voto4 + 1;
+        }
+    }
+    alert(
+        "Votos Candidato A: " + voto1 + "\n" +
+        "Votos candidato B: " + voto2 + "\n" +
+        "Votos Brancos: " + voto3 + "\n" +
+        "Votos nulos: " + voto4
+    )
+}
+
+function exercicio49ForCalcularPotencia(){
+    let base = parseInt(prompt("Digite a base"))
+    let expoente = parseInt(prompt("Digite o expoente"))
+    let resultado = 1;
+
+    for(let index = 0; index < Math.abs(expoente); index = index + 1){
+        resultado = resultado * base;
+    }
+
+    if(expoente < 0){
+        resultado = 1 / resultado;
+    }
+    console.log(resultado);
+}
+
+function exercicio50ForSequenciaCrescenteEDecrescente(){
+    let numero = parseInt(prompt("Digite o numero"));
+
+    for(let indice = 1; indice <= numero; indice = indice + 1){
+        console.log(indice);
+    }
+
+    for(let indice = numero; indice > 1; indice = indice - 1){
+        console.log(indice);
+    }
+    console.log("Sequência finalizada");
+}
+
+
+function exercicio51ForContarMultiplosEmIntervalo(){
+    let numeroInicial = parseInt(prompt("Digite o numero inicial"));
+    let numeroFinal = parseInt(prompt("Digite o numero final"));
+    let numeroDivisor = parseInt(prompt("Digite o numero divisor"));
+    let contadorMultiplos = 0;
+
+    for(let index = numeroInicial; index <= numeroFinal; index = index + 1){
+        if(numeroDivisor % index === 0){
+            contadorMultiplos = contadorMultiplos + 1;
+        }
+    }
+    console.log(contadorMultiplos);
+}
+
+function exercicio52ForRelatorioPagamentoFuncionarios(){
+    let contadorFuncionarios = 0;
+    let somaTotalFolha = 0;
+    let maiorSalario = 0;
+    let nomeMaiorSalario = 0;
+
+    for(let index = 0; index < 6; index = index + 1){
+        let nome = prompt("Digite o seu nome");
+        let horasTrabalhadas = parseInt(prompt("Digite as horas trabalhadas"));
+        let valorHoras = parseInt(prompt("Digite o valor por hora"));
+
+        contadorFuncionarios = contadorFuncionarios + 1;
+
+        let salario = horasTrabalhadas * valorHoras;
+
+        somaTotalFolha = somaTotalFolha + salario;
+
+        if(salario > maiorSalario){
+            maiorSalario = salario;
+            nomeMaiorSalario = nome;
+        }
+    }
+    console.log(
+        "Quantidade de funcionarios cadastrados: " + contadorFuncionarios + "\n" +
+        "Soma total da folha: " + somaTotalFolha + "\n" +
+        "Maior salario: " + maiorSalario + "\n" +
+        "Nome do funcionario com maior salario: " + nomeMaiorSalario
+    );
+}
+
+function exercicio53ForValidarSenhaTentativas(){
+
+    for(let index = 0; index < 4; index = index + 1){
+        let senha = prompt("Digite a senha");
+
+        if(senha === "0107"){
+            alert("Acesso permitido");
+            break;
+        }
+        if(index === 3){
+            alert("Acesso bloqueado");
+        }
     }
 }
